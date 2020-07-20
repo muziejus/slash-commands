122363fefc3d2e3d614630efdea4b72ee5d7c45f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-07-20 12:05:41 -0400

    Implement temperature conversion

--- express/server.js
+++ express/server.js
@@ -8,13 +8,24 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Please use POST</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.post('/', (req, res) => {
+  const {command, text} = req.body;
+  if(command === "/degrees") {
+    const number = parseInt(text.split(" ")[0], 10);
+    return res.json({
+      "response_type": "in_channel",
+      "text": `“${number}” could be ${(number - 32) * 5/9}°C or ${(number * 9/5) + 32}°F. Use context to decide.`
+    });
+  }
 
-app.use(bodyParser.json());
+  return res.json({ text: "I couldn't parse that request" });
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
